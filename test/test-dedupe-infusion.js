f3fdf5d2e3e1127d9506555e929858b417ce8217
Colin Clark <colinbdclark@gmail.com>
2015-10-08 16:27:06 -0400

    GPII-1259: Updates license URL and copyright statement. Adds JSON to jshint globals.

--- test/test-dedupe-infusion.js
+++ test/test-dedupe-infusion.js
@@ -1,13 +1,13 @@
 /*
 Dedupe Infusion
 
-Copyright 2015 Raising the Floor (International)
+Copyright 2015 Raising the Floor - International
 
 Licensed under the New BSD license. You may not use this file except in
 compliance with this License.
 
 You may obtain a copy of the License at
-https://github.com/gpii/kettle/LICENSE.txt
+https://github.com/fluid/dedupe-infusion/LICENSE.txt
 */
 
 "use strict";
